9d4c349406245ee0d9bee842bbcc07cc92cd7f6f
Kyle Fick <67210240+SharkTaleLover@users.noreply.github.com>
2023-04-14 09:29:54 -0400

    more added to convergent series proof

--- convergent_series.py
+++ convergent_series.py
@@ -13,3 +13,24 @@ class ConvergentSeries(Scene):
         gsumTex[0].set_color(WHITE)
         self.play(TransformMatchingTex(asumTex, gsumTex))
         self.wait(1)
+        
+        fgsum_tex = MathTex(r'\sum_{n=0}^{N}', r'{{a}}{{r}}^{{n}}').set_color_by_tex('a', RED).set_color_by_tex('r', color=GREEN).set_color_by_tex('n', YELLOW)
+        fgsum_tex[0].set_color(WHITE)
+        self.play(TransformMatchingTex(gsumTex, fgsum_tex))
+        self.wait(1)
+        
+        expanded_sum = MathTex(r'{{a}}', '+', r'{{a}}{{r}}', '+', r'\cdots', '+', r'{{a}}{{r}}^{{{N}} - 1}', color=WHITE).set_color_by_tex('a', RED).set_color_by_tex('r', GREEN)
+        eq_sign = MathTex('=')
+        g_series = Group(fgsum_tex, eq_sign, expanded_sum)
+        self.play(FadeIn(eq_sign), FadeIn(expanded_sum), g_series.animate.arrange_submobjects())
+        self.wait(1)
+        
+        ofgsum_tex = fgsum_tex.copy()
+        oexpanded_sum = expanded_sum.copy()
+        nfgsum_tex = MathTex(r'{{r}}', '\sum_{n=0}^{N}', r'{{a}}{{r}}^{{n}}').set_color_by_tex('a', RED).set_color_by_tex('r', color=GREEN).set_color_by_tex('n', YELLOW)
+        nfgsum_tex[1].set_color(WHITE)
+        nexpanded_sum = MathTex(r'{{a}}{{r}}', '+', r'{{a}}{{r}}^2', '+', r'\cdots', '+', r'{{a}}{{r}}^{{{N}}}', color=WHITE).set_color_by_tex('a', RED).set_color_by_tex('r', GREEN)
+        
+        self.play(TransformMatchingTex(fgsum_tex, nfgsum_tex), TransformMatchingTex(expanded_sum, nexpanded_sum))
+        self.wait(1)
+        
