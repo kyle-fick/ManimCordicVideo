cdacd2a526e7aaf27fb0897033b5ac940b260fbf
SharkTaleLover <kylecfick@gmail.com>
2023-05-10 20:24:40 -0400

    minor changes to atan_series.py

--- atan_series.py
+++ atan_series.py
@@ -9,10 +9,17 @@ class AtanSeries(Scene):
         self.wait(1)
 
         angle_series = MathTex(r'\approx', r'45', r'+', r'22.625', r'+', r'\cdots')
-        self.play(atan_series.animate.shift(UP*2), Write(angle_series.shift(DOWN*0.5)))
+        self.play(atan_series.animate.shift(UP*2), Write(angle_series))
         self.wait(1)
 
-        # not showing right abs value side for some reason
         ratio_test = MathTex(r'\lim_{n\to\infty}', r'\left| a_{n+1} \over a_{n} \right|', r'<', r'1')
         self.play(Write(ratio_test.shift(DOWN * 2)))
         self.wait(1)
+
+        ratio_rect = SurroundingRectangle(ratio_test, color=RED, buff=MED_LARGE_BUFF)
+        self.play(Create(ratio_rect))
+        self.wait(1)
+
+        self.play(FadeOut(atan_series), FadeOut(angle_series), FadeOut(ratio_test), FadeOut(ratio_rect))
+        self.wait(1)
+
