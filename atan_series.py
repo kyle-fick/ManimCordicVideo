3ac0d7137cb27cec5ecddd0ccde5317ed4dc1bf0
SharkTaleLover <kylecfick@gmail.com>
2023-05-11 10:47:42 -0400

    additions to atan_series.py

--- atan_series.py
+++ atan_series.py
@@ -20,7 +20,25 @@ class AtanSeries(Scene):
         self.play(Create(ratio_rect))
         self.wait(1)
 
-        self.play(FadeOut(atan_series), FadeOut(angle_series), FadeOut(ratio_test), FadeOut(ratio_rect))
+        self.play(FadeOut(atan_series), FadeOut(angle_series), FadeOut(ratio_rect), ratio_test.animate.move_to(ORIGIN))
         self.wait(1)
 
-        
+        ratio_test_2 = MathTex(r'\lim_{n\to\infty}', r'\left| \tan^{-1} \left( \frac{1}{2^{n+1}} \right) \over \tan^{-1} \left( \frac{1}{2^n} \right) \right|').scale(1.5)
+        self.play(Transform(ratio_test, ratio_test_2))
+        self.wait(1)
+
+        ratio_test_3 = MathTex(r'\lim_{n\to\infty}', r'\left| \frac{2^{n+1} \ln(2)}{1 + 4^{n+1}} \over \frac{2^n \ln(2)}{1 + 4^n} \right|').scale(1.5)
+        self.play(Transform(ratio_test, ratio_test_3))
+        self.wait(1)
+
+        ratio_test_4 = MathTex(r'\lim_{n\to\infty}', r'2 \cdot \frac{4^n + 1}{4^{n+1} + 1}').scale(1.5)
+        self.play(Transform(ratio_test, ratio_test_4))
+        self.wait(1)
+
+        ratio_test_5 = MathTex(r'\lim_{n\to\infty}', r'\frac{1}{2} \cdot \frac{4^x + 1}{4^x + \frac{1}{4}}').scale(1.5)
+        self.play(Transform(ratio_test, ratio_test_5))
+        self.wait(1)
+
+        ratio_test_final = MathTex(r'\frac{1}{2}').scale(1.5)
+        self.play(Transform(ratio_test, ratio_test_final))
+        self.wait(1)
